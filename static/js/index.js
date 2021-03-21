93ebb6672eae2eb93eb304d9e546225bbf388653
My1 <git@mail.my1.dev>
2021-03-21 14:35:39 +0000

    fixes and stuff

--- static/js/index.js
+++ static/js/index.js
@@ -83,6 +83,7 @@ function updateStats(){
     console.log(response.data);
     response.data.notcem = response.data.currentBlock - response.data.cementedBlocks;
     response.data.totalblocks = response.data.currentBlock + response.data.uncheckedBlocks;
+    response.data.accs = parseInt(response.data.telemetry.account_count);
     document.getElementById("content").innerHTML = template(response.data);
     new ClipboardJS('#copyAccount');
   })
